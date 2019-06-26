02586d44a431d6bb51cb4ffe9e0f87cfb8303a5b
“Hindreen” <hndfaw@gmail.com>
2019-06-25 18:52:53 -0600

    Add form to add new students

---
+++ src/components/AddStudent.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import './AddStudent.css';
+
+class AddStudent extends Component {
+
+  state = {
+    name: '',
+    quote: '',
+    superlative: '',
+    photo: 'https://placekitten.com/200/300',
+  }
+
+  handleChange = (e) => {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  submitNewStudent = (e) => {
+    e.preventDefault();
+    let id = new Date().getTime();
+    const {name, quote, superlative, photo} = this.state;
+    let newStudent = {
+      id,
+      name,
+      quote,
+      superlative,
+      photo
+    }
+    this.props.addNewStudent(newStudent);
+    // this.resetInputs();
+  }
+
+  // resetInputs = () => {
+  //   this.setState({name: ''});
+  //   this.setState({quote: ''});
+  //   this.setState({superlative: ''});
+  // }
+
+  render() {
+    return (
+      <form className={`add-studnet-form ${this.props.hide ? 'flex' : 'hide'}`}>
+        <label>Name</label>
+        <input  onChange={this.handleChange} name="name" type="text" vlaue={this.state.name}/>
+        <label>Quote</label>
+        <input vlaue={this.state.quote} onChange={this.handleChange} name="quote" type="text"/>
+        <label>Superlative</label>
+        <input vlaue={this.state.superlative} onChange={this.handleChange} name="superlative" type="text"/>
+        <button onClick={this.submitNewStudent}>Add</button>
+      </form>
+    )
+  }
+}
+
+export default AddStudent
--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,60 @@
 import React, { Component } from 'react';
 import Cohort from './Cohort';
 import people from '../data/yearbook-data.js';
+import AddStudent from '../components/AddStudent';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      staff: people.staff
+      staff: people.staff,
+      students: people.students,
+      data: people.staff,
+      currentValue: 'staff',
+      displayForm: false,
     }
   }
 
+
+  selectTab = (e) => {
+    const {staff, students} = this.state;
+
+    if (e.target.name === 'staff') {
+      this.setState({data: staff});
+      this.setState({currentValue: 'staff'});
+      this.setState({displayForm: false});
+    } else {
+      this.setState({data: students});
+      this.setState({currentValue: 'students'});
+    }
+  }
+
+  showAddNewStudent = () => {
+    this.setState({displayForm: !this.state.displayForm});
+  }
+
+  addNewStudent = (newStudent) => {
+    this.setState({students: [...this.state.students, newStudent]})
+    this.setState({data: [...this.state.students, newStudent]})
+  }
+
   render() {
-    const {staff} = this.state
+
     return (
       <div className="App">
-      <header className="App-header">
-      <h1>Turing Yearbook</h1>
-      </header>
-        <Cohort staff={staff}/>
+        <header className="App-header">
+        <h1>Turing Yearbook</h1>
+        <div className="tabs-container">
+          <div className="inline-block">
+            <button  onClick={this.selectTab} name="staff" className={`btn ${this.state.currentValue === 'staff' ? "btn-dark" : "btn-light"}`} type="button" >Staff</button>
+            <button onClick={this.selectTab} name="students" className={`btn ${this.state.currentValue === 'students' ? "btn-dark" : "btn-light"}`} type="button" >Students</button>
+          </div>
+          <button onClick={this.showAddNewStudent} className={`${this.state.currentValue === 'students' ? "add-btn" : "hide"}`}>Add new student</button>
+        </div>
+        </header>
+        <AddStudent addNewStudent={this.addNewStudent} hide={this.state.displayForm}/>
+        <Cohort staff={this.state.data} />
       </div>
     );
   }
